221aad36aba6c899dce787e8876bd2e1b56f2881
Pearce Odegard <pearceodegard@gmail.com>
2022-04-28 12:47:19 -0500

    Initial commit

---
+++ InterfacePractice/Cat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace InterfacePractice
+{
+    // Create the IFeedable interface
+    // Follow the book to see what methods to put into the IFeedable interface
+    // Have the Cat class implement the IFeedable interface
+    // Implement the necessary methods defined in the interface
+    public class Cat
+    {
+        // This is an empty constructor, can be ignored for now
+        public Cat()
+        {
+        }
+    }
+}
